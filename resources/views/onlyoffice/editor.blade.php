785f254280b3a0eadaebdc395370b6e99d579302
wahid fkiri <wahidfkiri5@gmail.com>
2025-11-11 16:49:16 +0100

    update some errors

--- resources/views/onlyoffice/editor.blade.php
+++ resources/views/onlyoffice/editor.blade.php
@@ -4,13 +4,30 @@
     <meta charset="UTF-8">
     <title>OnlyOffice Editor</title>
     <script src="http://217.182.168.27:8080/web-apps/apps/api/documents/api.js"></script>
+    <style>
+        html, body {
+            margin: 0;
+            padding: 0;
+            height: 100%;
+            overflow: hidden; /* prevent scrolling */
+        }
+        #placeholder {
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            width: 100%;
+            height: 100%;
+        }
+    </style>
 </head>
-<body style="margin:0; padding:0;">
-    <div id="placeholder" style="width:100%; height:100vh;"></div>
+<body>
+    <div id="placeholder"></div>
 
     <script>
         const config = @json($config);
-        const docEditor = new DocsAPI.DocEditor("placeholder", config);
+        new DocsAPI.DocEditor("placeholder", config);
     </script>
 </body>
 </html>
